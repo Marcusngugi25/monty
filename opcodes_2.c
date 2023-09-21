253e40ff328489d9a12af951ca4cb792f3c97109
Marcusngugi25 <marcusngugi@gmail.com>
2023-09-21 15:55:08 -0700

    opcodes_2.c

--- opcodes_2.c
+++ opcodes_2.c
@@ -1,10 +1,12 @@
 #include "monty.h"
+
 /**
  *add_func - Giving the opcode add the ability to add the numbers stored
  *in the linked list and removing the first node on top of the stack.
  *@stack: The reference to the entire doubly linked list.
  *@num: Line number currently being evaluated.
  */
+
 void add_func(stack_t **stack, unsigned int num)
 {
 	stack_t *temp;
